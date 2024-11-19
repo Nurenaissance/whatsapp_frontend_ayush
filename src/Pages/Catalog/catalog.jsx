9a101e1982e6267bf3da352df69e58d84ed12811
Shreyas9548 <shreyasv828@gmail.com>
2024-11-19 08:59:31 +0530

    fixed css catalog

--- src/Pages/Catalog/catalog.jsx
+++ src/Pages/Catalog/catalog.jsx
@@ -12,6 +12,10 @@ const Catalog = () => {
   const [textToCopy, setTextToCopy] = useState({ display: "Copy Link", url: "" });
   const [catalogID, setCatalogID] = useState('')
   const [tableData, setTableData] = useState([]);
+  const [duplicateIndex, setDuplicateIndex] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [imageURLs, setImageURLs] = useState({});
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   const generateRandomProductID = () => {
     const characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
@@ -21,56 +25,51 @@ const Catalog = () => {
     }
     return result;
   };
+  
+  const fetchData = async () => {
+    try {
+      const django_promise = axiosInstance.post(`/create-spreadsheet/`);
+      const fastResponse = await axiosInstance.get(`${fastURL}/catalog`)
+      
+      const products = fastResponse.data
+      
+      const initialData = products.map(product => ({
+        product_id: product.product_id || generateRandomProductID(),
+        title: product.title || "",
+        description: product.description || "",
+        link: product.link || "",
+        image_link: product.image_link || "",
+        condition: product.condition || "",
+        quantity: product.quantity || "",
+        price: product.price || "",
+        brand: product.brand || "",
+        status: product.status || ""
+      })
+    );
+      console.log("Changing the Original Data...", initialData)
+      setOriginalData([...initialData.map(item => ({...item}))]);
+      setTableData(initialData);
+      console.log("Initial Data: ", initialData)
+      const initialImageURLs = {};
+      initialData.forEach((item, index) => {
+        if (item.image_link) {
+          initialImageURLs[index] = item.image_link;
+        }
+      });
+      setImageURLs(initialImageURLs);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const django_promise = axiosInstance.post(`/create-spreadsheet/`);
-        const fastResponse = await axiosInstance.get(`${fastURL}/catalog`)
-        
-        const products = fastResponse.data
-        
-        const initialData = products.map(product => ({
-          product_id: product.product_id || generateRandomProductID(),
-          title: product.title || "",
-          description: product.description || "",
-          link: product.link || "",
-          image_link: product.image_link || "",
-          condition: product.condition || "",
-          quantity: product.quantity || "",
-          price: product.price || "",
-          brand: product.brand || "",
-          status: product.status || ""
-        })
-      );
-        console.log("Changing the Original Data...", initialData)
-        setOriginalData([...initialData.map(item => ({...item}))]);
-        setTableData(initialData);
-        console.log("Initial Data: ", initialData)
-        const initialImageURLs = {};
-        initialData.forEach((item, index) => {
-          if (item.image_link) {
-            initialImageURLs[index] = item.image_link;
-          }
-        });
-        setImageURLs(initialImageURLs);
-
-        const djangoResponse = await django_promise
-        setTextToCopy(prevState => ({ ...prevState, url: djangoResponse.data.spreadsheet_url }));
-        setCatalogID(djangoResponse.data.catalog_id)
+      const djangoResponse = await django_promise
+      setTextToCopy(prevState => ({ ...prevState, url: djangoResponse.data.spreadsheet_url }));
+      setCatalogID(djangoResponse.data.catalog_id)
 
 
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      } finally{
-        setLoading(false)
-      }
-    };
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally{
+      setLoading(false)
+    }
+  };
 
-    fetchData();
-  }, []);
-
-  const [duplicateIndex, setDuplicateIndex] = useState(null);
 
   const handleCellChange = (e, rowIndex, field) => {
     const newData = [...tableData];
@@ -88,9 +87,6 @@ const Catalog = () => {
     }
   };
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [imageURLs, setImageURLs] = useState({});
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   const uploadToBlob = async (e, rowIndex, field) => {
     try {
@@ -209,7 +205,7 @@ const Catalog = () => {
   };
 
   const handleEditClick = async () => {
-    setIsPopupVisible(!isPopupVisible);
+  setIsPopupVisible((prev) => !prev);
   }
 
   const handleInputChange = (e) => {
@@ -224,171 +220,236 @@ const Catalog = () => {
     handleEditClick()
   }
 
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
-    <div className="bp-catalog">
-     <div 
-        className={`copy-text-box ${textToCopy.display === "Copied!" ? "copied" : ""}`} 
-        onClick={handleCopyText}
-      >
-        {textToCopy.display}
-      </div>
-      <div className="hidden lg:flex items-center input-catalogid">
-        <b>Catalog ID: {catalogID}</b>
-      <Edit style={{ position: 'relative', marginLeft: '12px', cursor: 'pointer' }} size={18} onClick={handleEditClick}/>
-      <div
-          className={`absolute mt-2 p-2 bg-white border border-gray-300 shadow-lg rounded popup ${
-            isPopupVisible ? 'popup-enter' : 'popup-exit'
-          }`}
-          style={{ top: '45px', left: '10px', transformOrigin: '85px -15px', borderRadius: '7px' }}
-        >
-          <p>Catalog ID:</p>
-          <input type="text" placeholder="Enter new Catalog ID" className="mt-2 p-1 border rounded" value={catalogID} onChange={handleInputChange}/>
-          <button onClick={handleSaveCatalogID} className="mt-2 p-1 bg-blue-500 text-white rounded">
-            Save
-          </button>
+    <div className="bp-catalog absolute right-0">
+
+      {/* Division: Catalog Management Section */}
+      <div className="catalog-management-section flex flex-col lg:flex-row items-start lg:items-center justify-between p-4 rounded-lg gap-4">
+
+
+
+        {/* Catalog ID */}
+        <div className="relative hidden lg:flex items-center input-catalogid">
+          <b>Catalog ID: {catalogID}</b>
+          <Edit 
+            style={{ position: 'relative', marginLeft: '12px', cursor: 'pointer' }} 
+            size={18} 
+            onClick={handleEditClick}
+          />
+          <div
+            className={`absolute mt-2 p-2 bg-white border border-gray-300 shadow-lg rounded popup ${
+              isPopupVisible ? 'popup-enter' : 'popup-exit'
+            }`}
+            style={{ top: '45px', left: '10px', transformOrigin: '85px -15px', borderRadius: '7px' }}
+          >
+            <p>Catalog ID:</p>
+            <input 
+              type="text" 
+              placeholder="Enter new Catalog ID" 
+              className="mt-2 p-1 border rounded" 
+              value={catalogID} 
+              onChange={handleInputChange}
+            />
+            <button 
+              onClick={handleSaveCatalogID} 
+              className="mt-2 p-1 bg-blue-500 text-white rounded"
+            >
+              Save
+            </button>
+          </div>
         </div>
+
+        {/* Title */}
+        <h1
+          className="text-2xl md:text-3xl font-semibold text-center mt-4 lg:mt-0 flex-grow"
+        >
+          Catalog Management
+        </h1>
+
+
+        {/* Copy Link */}
+        <div 
+          className={`copy-text-box ${textToCopy.display === "Copied!" ? "copied" : ""}`} 
+          onClick={handleCopyText}
+          style={{ marginBottom: '10px' }} // Add spacing for mobile
+        >
+          {textToCopy.display}
+        </div>
+        
+
       </div>
 
-      <h1 style={{ fontSize: '36px', fontWeight: '600', fontFamily: 'sans-serif', paddingBottom: 20, alignContent: 'center' }}>Catalog Management</h1>
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table id="dt" className="w-full text-left border border-gray-300">
+  <thead>
+    <tr className="bg-gray-200">
+      <th></th>
+      <th>Product ID</th>
+      <th>Product Name</th>
+      <th>Description</th>
+      <th>Product Link</th>
+      <th>Image</th>
+      <th>Condition</th>
+      <th>Quantity</th>
+      <th>Item Price</th>
+      <th>Brand</th>
+      <th>Status</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {tableData.map((row, rowIndex) => (
+      <tr
+        key={rowIndex}
+        className={`${
+          duplicateIndex === rowIndex ? "duplicate-input" : ""
+        } odd:bg-white even:bg-gray-100 hover:bg-gray-200`}
+      >
+        <td>{rowIndex + 1}</td>
+        <td>
+          <input
+            type="text"
+            value={row.product_id || ""}
+            onChange={(e) => handleCellChange(e, rowIndex, "product_id")}
+            className="column-bg"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={row.title}
+            placeholder="Product Name"
+            onChange={(e) => handleCellChange(e, rowIndex, "title")}
+            className="column-bg"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={row.description}
+            placeholder="Description"
+            onChange={(e) => handleCellChange(e, rowIndex, "description")}
+            className="column-bg"
+          />
+        </td>
+        <td>
+          <input
+            type="url"
+            value={row.link}
+            placeholder="Product Link"
+            onChange={(e) => handleCellChange(e, rowIndex, "link")}
+            className="column-bg"
+          />
+        </td>
+        <td className="flex items-center space-x-4">
+          {imageURLs[rowIndex] && (
+            <img
+              src={imageURLs[rowIndex]}
+              alt="Preview"
+              style={{ width: "70px", height: "70px" }}
+            />
+          )}
+          <input
+            type="file"
+            accept="image/"
+            placeholder="Image"
+            onChange={(e) => uploadToBlob(e, rowIndex, "image_link")}
+            className="column-bg"
+          />
+        </td>
+        <td>
+          <select
+            value={row.condition}
+            onChange={(e) => handleCellChange(e, rowIndex, "condition")}
+            className="column-bg"
+          >
+            <option value="new">New</option>
+            <option value="used">Used</option>
+            <option value="refurbished">Refurbished</option>
+          </select>
+        </td>
+        <td>
+          <input
+            type="number"
+            value={row.quantity}
+            placeholder="Quantity"
+            onChange={(e) => handleCellChange(e, rowIndex, "quantity")}
+            className="column-bg"
+            min="0"
+          />
+        </td>
+        <td>
+          <input
+            type="number"
+            value={row.price}
+            placeholder="Price"
+            onChange={(e) => handleCellChange(e, rowIndex, "price")}
+            className="column-bg"
+            min="0"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            value={row.brand}
+            placeholder="Brand Name"
+            onChange={(e) => handleCellChange(e, rowIndex, "brand")}
+            className="column-bg"
+          />
+        </td>
+        <td>
+          <select
+            value={row.status}
+            onChange={(e) => handleCellChange(e, rowIndex, "status")}
+            className="column-bg"
+          >
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+          </select>
+        </td>
+        <td>
+          <button
+            className="btn btn-delete"
+            title="Delete"
+            onClick={() => deleteRow(rowIndex)}
+          >
+            <Trash2 className="trash-icon" size={18} />
+          </button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
 
-      <table border="1" style={{ width: "125%", textAlign: "left", fontSize: 14 }}>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Product ID</th>
-            <th>Product Name</th>
-            <th>Description</th>
-            <th>Product Link</th>
-            <th>Image</th>
-            <th>Condition</th>
-            <th>Quantity</th>
-            <th>Item Price</th>
-            <th>Brand</th>
-            <th>Status</th>
-            <th>Delete Row</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row, rowIndex) => (
-            <tr key={rowIndex} className={duplicateIndex === rowIndex ? "duplicate-input" : ""}>
-              <td>{rowIndex + 1}</td>
-              <td>
-                <input
-                  type="text"
-                  value={row.product_id || ""}
-                  onChange={(e) => handleCellChange(e, rowIndex, "product_id")}
-                  className="column-bg"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={row.title}
-                  placeholder="Product Name"
-                  onChange={(e) => handleCellChange(e, rowIndex, "title")}
-                  className="column-bg"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={row.description}
-                  placeholder="Description"
-                  onChange={(e) => handleCellChange(e, rowIndex, "description")}
-                  className="column-bg"
-                />
-              </td>
-              <td>
-                <input
-                  type="url"
-                  value={row.link}
-                  placeholder="Product Link"
-                  onChange={(e) => handleCellChange(e, rowIndex, "link")}
-                  className="column-bg"
-                />
-              </td>
-              <td className="flex items-center space-x-4">
-                
-              {imageURLs[rowIndex] && (
-                  <img src = {imageURLs[rowIndex]} alt="Preview" style={{width: "70px", height: "70px"}}/>
-                )}
-                <input
-                  type="file"
-                  accept="image/"
-                  placeholder="Image"
-                  onChange={(e) => uploadToBlob(e, rowIndex, "image_link")}
-                  className="column-bg"
-                />
-              </td>
-              <td>
-                <select
-                  value={row.condition}
-                  onChange={(e) => handleCellChange(e, rowIndex, "condition")}
-                  className="column-bg"
-                //   defaultValue="new"
-                >
-                  <option value="new">New</option>
-                  <option value="used">Used</option>
-                  <option value="refurbished">Refurbished</option>
-                </select>
-              </td>
-              <td>
-              <input
-                  type="number"
-                  value={row.quantity}
-                  placeholder="Quantity"
-                  onChange={(e) => handleCellChange(e, rowIndex, "quantity")}
-                  className="column-bg"
-                  min="0"
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={row.price}
-                  placeholder="Price"
-                  onChange={(e) => handleCellChange(e, rowIndex, "price")}
-                  className="column-bg"
-                  min="0"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={row.brand}
-                  placeholder="Brand Name"
-                  onChange={(e) => handleCellChange(e, rowIndex, "brand")}
-                  className="column-bg"
-                />
-              </td>
-              <td>
-                <select
-                  value={row.status}
-                  onChange={(e) => handleCellChange(e, rowIndex, "status")}
-                  className="column-bg"
-                //   defaultValue="active"
-                >
-                  <option value="active">Active</option>
-                  <option value="inactive">Inactive</option>
-                </select>
-              </td>
-              <td>
-                <button className="btn btn-delete" onClick={() => deleteRow(rowIndex)}>
-                  <Trash2 className="trash-icon" size={18} />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      </div>
+
+      {/* Division: Submit Button */}
       <div className="submit-container">
-    <button className="submit-button" onClick={handleSubmitCatalog}>
+    <button className="submit-button px-6 py-2 bg-green-500 text-white rounded-lg text-base lg:text-lg hover:bg-green-600 transition duration-200" onClick={handleSubmitCatalog}>
       {isSubmitting? 'Submitting...': 'Submit'}
     </button>
-  </div>
-      <button className="add-row-button" onClick={addRow}>+</button>
+      </div>
+
+      {/* Add Row Button */}
+      <button 
+        className="add-row-button fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full text-lg shadow-lg hover:bg-blue-600 lg:bottom-6 lg:right-6 lg:p-5"
+        onClick={addRow}
+      >
+        +
+      </button>
+
+
     </div>
+    
   );
+  
 }
 
 export default Catalog;
